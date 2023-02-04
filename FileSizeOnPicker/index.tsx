494d51b9436f9bec6389862e22fedeaf07cb4edd
Martinz64 <martinz@martinz.dev>
2023-02-04 10:44:15 +0100

    [FileSizeOnPicker]: fix log spam

--- FileSizeOnPicker/index.tsx
+++ FileSizeOnPicker/index.tsx
@@ -27,47 +27,55 @@ export default class FileSizeOnPicker extends Plugin {
         this.patcher.after(getByProps("VirtualizedListCellContextProvider"),"VirtualizedListCellContextProvider",(ctx,component:any)=>{
             //console.log("VirtualizedListCellContextProvider",component)
             //window.upload1=component
-            if(component.props?.children?.props?.children[0]?.type){
-                this.patcher.after(component.props.children.props.children[0].type,"type",(ctx,component:any)=>{
-                    //console.log("VirtualizedListCellContextProvider.TYPE",component)
-                    //window.upload3=component
+            const firstChild = component.props?.children?.props?.children[0]
+            if(firstChild){
+                if(firstChild.type?.type){
+                    console.log("type1")
+                    this.patcher.after(firstChild.type,"type",(ctx,component:any)=>{
+                        //console.log("VirtualizedListCellContextProvider.TYPE",component)
+                        window.upload3=component
 
-                    //fix lag on emoji picker
-                    if(component.props?.onPress?.name == "onCategoryPress") return; 
+                        //fix lag on emoji picker
+                        //if(component.props?.onPress?.name == "onCategoryPress") return; 
+                        if(!component.props?.children[0]?.props?.item?.node?.image) return;
 
-                    component.props?.children?.forEach(child => {
-                        if(child.type)this.patcher.after(child,"type",(ctx,component: any) => {
-                            if(!component.props?.patched){
-                                //window.upload4=component
-                                const imageInfo = component.props?.children[0]?.props?.source
-                                console.log(imageInfo.uri)
-                                if(imageInfo && component.props.children){
-                                    if(imageInfo.uri){
-                                        console.log(imageInfo.uri)
-                                        component.props.children.push(
-                                            <View style={{
-                                                position: 'absolute',
-                                                opacity: 1,
-                                                backgroundColor: 'rgb(55,55,55)',
-                                                padding: 4,
-                                                paddingLeft: 8,
-                                                paddingRight: 8,
-                                                top: 4,
-                                                left: 4,
-                                                borderRadius: 4,
-                                            }}>
-                                                <SizeTag url={imageInfo.uri} />
-                                            </View>
-                                        )
+                        component.props?.children?.forEach(child => {
+                            if(child){
+                                console.log("type2")
+                                this.patcher.after(child,"type",(ctx,component: any) => {
+                                    if(!component.props?.patched){
+                                        //window.upload4=component
+                                        const imageInfo = component.props?.children[0]?.props?.source
+                                        //console.log(imageInfo.uri)
+                                        if(imageInfo && component.props.children){
+                                            if(imageInfo.uri){
+                                                //console.log(imageInfo.uri)
+                                                component.props.children.push(
+                                                    <View style={{
+                                                        position: 'absolute',
+                                                        opacity: 1,
+                                                        backgroundColor: 'rgb(55,55,55)',
+                                                        padding: 4,
+                                                        paddingLeft: 8,
+                                                        paddingRight: 8,
+                                                        top: 4,
+                                                        left: 4,
+                                                        borderRadius: 4,
+                                                    }}>
+                                                        <SizeTag url={imageInfo.uri} />
+                                                    </View>
+                                                )
+                                            }
+                                        }
+                                        component.props.patched = true
                                     }
-                                }
-                                component.props.patched = true
+                                    //window.upload4=component
+                                })
                             }
-                            //window.upload4=component
                         })
-                    })
 
-                })
+                    })
+                }
             }
         })
     }
