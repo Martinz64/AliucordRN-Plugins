c3203036a50abb98951362a13cefa4557ee2e0ef
Martinz64 <martinz@martinz.dev>
2023-02-05 16:32:50 +0100

    [DoubleTapReply]: clean up

--- DoubleTapReply/index.tsx
+++ DoubleTapReply/index.tsx
@@ -1,8 +1,6 @@
 import { Plugin } from 'aliucord/entities';
 // @ts-ignore
-import { getModule, Toasts, React, ReactNative, Dialog, Locale, getByProps, MessageStore, ChannelStore } from 'aliucord/metro';
-import { getAssetId } from 'aliucord/utils/getAssetId';
-import { after } from "aliucord/utils/patcher";
+import { getModule, getByProps, MessageStore, ChannelStore } from 'aliucord/metro';
 
 type FilterOptions = {
     exports?: boolean;
@@ -12,38 +10,11 @@ type FilterOptions = {
     default?: true;
 };
 
-
-
 export default class DoubleTapReply extends Plugin {
     private getByName(defaultName: string, options?: FilterOptions) {
         return getModule(m => m?.default?.name === defaultName, options);
     }
     public async start() {
-        const EMOJI_REGEX = /https?:\/\/.*\/emojis\/([0-9]+).(webp|png|gif)\?.*/
-
-
-        //ReactNative.requireNativeComponent
-        /*this.patcher.before(ReactNative,"requireNativeComponent",(ctx) => {
-            console.log("requireNativeComponent",ctx.args,ctx.result)
-        })*/
-
-        this.patcher.before(getByProps("DCDChat"),"createView",(ctx) => {
-            //console.log("createView",ctx.args,ctx.result)
-        })
-
-        this.patcher.after(this.getByName("ChatManager"),"default",(ctx) => {
-            console.log("ChatManager",ctx.args,ctx.result)
-        })
-
-        this.patcher.after(this.getByName("ChatViewConnected"),"default",(ctx) => {
-            console.log("ChatViewConnected",ctx.args,ctx.result)
-            const component = ctx.result
-            this.patcher.after(component,"type",(ctx) => {
-                console.log("ChatViewConnected.type",ctx.args,ctx.result)
-
-            })
-        })
-
         const ReplyManager = getByProps("createPendingReply")
 
         const Chat = this.getByName("Chat")
